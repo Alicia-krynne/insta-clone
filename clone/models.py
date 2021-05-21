be8908787d6e9a4a34a01e1bcb8723e091af2a3f
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-21 20:55:02 +0300

    add profile model

--- clone/models.py
+++ clone/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 #from django.db.models import Q
 
+
+Gender=(
+    ('Male','Male'),
+    ('Female','Female'),
+)
 # Create your models here.
 class Image(models.Model):
     image= models.ImageField(upload_to="media/")
@@ -13,14 +18,34 @@ class Image(models.Model):
     comments=models.TextField(max_length=600)
     
     def __str__(self):
-        return self.title
+        return self.name
     
     def save_image(self):
           self.save()
         
-    def update_image(self):
+    def update_caption(self):
         self.save()
 
     def delete_image(self):
         self.delete()
+
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to='profilepics/')
+    bio = HTMLField()
+    name = models.CharField(max_length=255)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    email= models.EmailField()
+    phonenumber = models.IntegerField()
+    gender = models.CharField(max_length=15,choices=Gender,default="Male")
+
+    def __str__(self):
+        return self.name
+    
+    def save_profile(self):
+          self.save()
         
+    def update_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
