5323a21c5e9a6925779eed9bbc6c9157eef63762
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-21 20:48:27 +0300

    test  for image methods added

--- clone/tests.py
+++ clone/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Image
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.image = Image(id=1,name="Slide Away",caption="nice place to  call home",comments= "pretty" ,likes= 2)
+        self.image.save_image()
+    
+    def test_save_image(self):
+        self.image.save_image()
+        image = Image.objects.all()
+        self.assertTrue(len(image) > 0)
+
+
+    def test_delete_image(self):
+        self.image.delete_image()
+        image=Image.objects.all()
+        self.assertTrue(len(image)==0)
+
+    def test_update_image(self):
+        self.image.update_image()
+        image=Image.objects.all()
+        self.assertTrue(len(image)>1)
