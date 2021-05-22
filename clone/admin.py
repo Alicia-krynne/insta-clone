9ac5bd9b7e5e34a6857f563f84832235df8d1c28
Alicia-krynne <alicemacrineadhiambo@gmail.com>
2021-05-22 23:48:19 +0300

    add urls and register

--- clone/admin.py
+++ clone/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import  Profile,Image,Comment
 # Register your models here.
+
+
+#class ArticleAdmin(admin.ModelAdmin):
+    #filter_horizontal =('tags',)
+
+
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Comment)
--- clone/urls.py
+++ clone/urls.py
@@ -1,24 +1,28 @@
-from django.conf.urls import url,include
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-
 urlpatterns=[
-    url(r'^$',views.timeline,name='Timeline'),
-    url(r'^search/',views.search_results,name='search_results'),
-    url(r'^new/post$',views.new_post, name='new-post'),
+    path('',views.homepage,name='Homepage'),
+    path('search/',views.search_results,name='search_results'),
+    path('new/image',views.new_image, name='new-image'),
+    path ('profile/',views.profile,name='profile'),
+    path('comment/',views.comment,name='comment'),
+    path('like/', views.like, name='like'),
+    
+
     #url(r'^new/location$',views.new_location, name='new-location'),
-    url(r'^profile/',views.profile, name='profile'),
-    url(r'^edit/profile$',views.edit_profile, name='edit-profile'),
-    url(r'^explore/',views.explore, name='explore'),
-    url(r'^like/$', views.like, name='like'),
-    url(r'^search/',views.search_results, name='search_results'),
-    url(r'^user-profile/(\d+)',views.userprofile,name='user-profile'),
-    # url(r'^user-profile/(?P<username>\w{0,50})',views.userprofile,name='user-profile'),
-    url(r'^comment/$',views.comment,name='comment'),
-    url(r'^change_profile/(?P<username>\w{0,50})',views.change_profile,name='change_profile'),
-]
+#     url(r'^profile/',views.profile, name='profile'),
+#     url(r'^edit/profile$',views.edit_profile, name='edit-profile'),
+#     url(r'^explore/',views.explore, name='explore'),
+#     url(r'^like/$', views.like, name='like'),
+#     url(r'^search/',views.search_results, name='search_results'),
+#     url(r'^user-profile/(\d+)',views.userprofile,name='user-profile'),
+#     # url(r'^user-profile/(?P<username>\w{0,50})',views.userprofile,name='user-profile'),
+#     
+#     url(r'^change_profile/(?P<username>\w{0,50})',views.change_profile,name='change_profile'),
+ ]
 
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
